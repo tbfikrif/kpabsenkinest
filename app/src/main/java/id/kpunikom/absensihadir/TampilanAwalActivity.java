b3928eded952c1273990a14f22e4d76e4a058672
Alesna <alesna97@gmail.com>
2018-07-08 19:39:15 +0700

    LANJUTKAN FIK !

--- app/src/main/java/id/kpunikom/absensihadir/TampilanAwalActivity.java
+++ app/src/main/java/id/kpunikom/absensihadir/TampilanAwalActivity.java
@@ -1,7 +1,12 @@
 package id.kpunikom.absensihadir;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import at.markushi.ui.CircleButton;
 
 public class TampilanAwalActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class TampilanAwalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan_awal);
+
+        CircleButton btnScanner = findViewById(R.id.btnscanner);
+         btnScanner.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent TampilanScannerActivity = new
+                         Intent(TampilanAwalActivity.this, TampilanScannerActivity.class);
+                 startActivity(TampilanScannerActivity);
+             }
+         });
+
     }
 }
