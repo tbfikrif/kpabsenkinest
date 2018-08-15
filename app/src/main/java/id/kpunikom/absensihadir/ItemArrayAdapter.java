a684f3dd67053d9c4092b59e463b026ee27d1816
tbfikrif <tbfikrif@gmail.com>
2018-08-15 13:40:31 +0700

    -Foto Profil -Nomor Urut -Error Handling (No Connection)

--- app/src/main/java/id/kpunikom/absensihadir/ItemArrayAdapter.java
+++ app/src/main/java/id/kpunikom/absensihadir/ItemArrayAdapter.java
@@ -1,12 +1,20 @@
 package id.kpunikom.absensihadir;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import id.kpunikom.absensihadir.model.Item;
@@ -16,6 +24,7 @@ public class ItemArrayAdapter extends RecyclerView.Adapter<ItemArrayAdapter.View
     //All methods in this adapter are required for a bare minimum recyclerview adapter
     private int listItemLayout;
     private ArrayList<Item> itemList;
+    Bitmap bitmap;
     // Constructor of the class
     public ItemArrayAdapter(int layoutId, ArrayList<Item> itemList) {
         listItemLayout = layoutId;
@@ -40,25 +49,35 @@ public class ItemArrayAdapter extends RecyclerView.Adapter<ItemArrayAdapter.View
     // load data in each row element
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int listPosition) {
+        TextView noUrut = holder.noUrut;
         TextView nama = holder.nama;
         TextView email = holder.email;
         TextView jam_masuk = holder.jam_masuk;
+        ImageView foto = holder.foto;
+        int temp = listPosition + 1;
+
+        noUrut.setText(Integer.toString(temp));
         nama.setText(itemList.get(listPosition).getNama());
         email.setText(itemList.get(listPosition).getEmail());
         jam_masuk.setText(itemList.get(listPosition).getJam_masuk());
+        new GetImageFromURL(foto).execute("http://192.168.1.32/kakatu/dist/fotoprofile/"+itemList.get(listPosition).getFoto());
     }
 
     // Static inner class to initialize the views of rows
     static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView noUrut;
         public TextView nama;
         public TextView email;
         public TextView jam_masuk;
+        public ImageView foto;
         public ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
+            noUrut = itemView.findViewById(R.id.txNoUrut);
             nama = itemView.findViewById(R.id.nama);
             email = itemView.findViewById(R.id.email);
             jam_masuk = itemView.findViewById(R.id.jam_masuk);
+            foto = itemView.findViewById(R.id.foto_profil);
         }
         @Override
         public void onClick(View view) {
@@ -70,5 +89,33 @@ public class ItemArrayAdapter extends RecyclerView.Adapter<ItemArrayAdapter.View
         itemList.clear();
         itemList.addAll(newItems);
         this.notifyDataSetChanged();
+    }
+
+    //Class for download IMAGE
+    public class GetImageFromURL extends AsyncTask<String, Void, Bitmap> {
+        ImageView imgV;
+
+        public GetImageFromURL(ImageView imgV){
+            this.imgV = imgV;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            String urldisplay = url[0];
+            bitmap = null;
+            try {
+                InputStream srt = new java.net.URL(urldisplay).openStream();
+                bitmap = BitmapFactory.decodeStream(srt);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imgV.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/id/kpunikom/absensihadir/TampilanAwalActivity.java
+++ app/src/main/java/id/kpunikom/absensihadir/TampilanAwalActivity.java
@@ -1,6 +1,11 @@
 package id.kpunikom.absensihadir;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +18,51 @@ public class TampilanAwalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan_awal);
-         Button btnScanner = findViewById(R.id.btn_scanner);
-         btnScanner.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent TampilanScannerActivity = new
-                         Intent(TampilanAwalActivity.this, TampilanScannerActivity.class);
-                 startActivity(TampilanScannerActivity);
-             }
-         });
+
+        Button btnScanner = findViewById(R.id.btn_scanner);
+
+        btnScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent TampilanScannerActivity = new
+                        Intent(TampilanAwalActivity.this, TampilanScannerActivity.class);
+                startActivity(TampilanScannerActivity);
+            }
+        });
+
+        if (!isConnected(TampilanAwalActivity.this)) {
+            buildDialog(TampilanAwalActivity.this).show();
+        }
 
     }
 
+    public boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if ((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting()))
+                return true;
+            else
+                return false;
+        } else
+            return false;
+    }
+
+    public AlertDialog.Builder buildDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("You need to have Mobile Data or Wifi to access this. Press Ok to Exit");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        return builder;
+    }
 }
